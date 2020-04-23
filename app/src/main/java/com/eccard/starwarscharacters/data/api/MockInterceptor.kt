8d9ae12fc449a246235c5c7ccaf3652c9cab39b8
eccard <felipelugao@id.uff.br>
2020-04-23 16:15:55 -0300

    Fill datas

--- app/src/main/java/com/eccard/starwarscharacters/data/api/MockInterceptor.kt
+++ app/src/main/java/com/eccard/starwarscharacters/data/api/MockInterceptor.kt
@@ -55,7 +55,7 @@ const val getCharacters = """
  	"items": [{
  			"id": 0,
  			"isMain": true,
- 			"name": "Anakin Skywalker/Darth Vader",
+ 			"name": "Anakin Skywalker,Darth Vader",
  			"image_url": "https://upload.wikimedia.org/wikipedia/en/7/76/Darth_Vader.jpg",
  			"gender": "male",
  			"first_appearance": "Star Wars (film)"
@@ -209,7 +209,7 @@ const val getCharacters = """
  			"isMain": true,
  			"name": "Darth Maul",
  			"image_url": "https://upload.wikimedia.org/wikipedia/en/b/bf/Darth_Maul.png",
- 			"gender": ""
+ 			"gender": "male"
  		},
  		{
  			"id": 20,
@@ -297,7 +297,7 @@ const val getCharacters = """
  		{
  			"id": 30,
  			"isMain": true,
- 			"name": "Líder Supremo Snoke",
+ 			"name": "Supreme Leader Snoke",
  			"image_url": "https://upload.wikimedia.org/wikipedia/en/thumb/4/4f/Snoke-The_Force_Awakens_%282015%29.png/330px-Snoke-The_Force_Awakens_%282015%29.png",
  			"gender": "male",
  			"first_appearance": "The Force Awaken",
@@ -342,8 +342,8 @@ const val getCharacters = """
  			"id": 36,
  			"isMain": false,
  			"name": "Bail Organa",
- 			"image_url": "",
- 			"gender": ""
+ 			"image_url": "https://m.media-amazon.com/images/M/MV5BMTMxNzI5OTY3NF5BMl5BanBnXkFtZTcwOTEwNTI4NA@@._V1_SY1000_CR0,0,651,1000_AL_.jpg",
+ 			"gender": "male"
  		},
  		{
  			"id": 37,
@@ -476,7 +476,7 @@ const val getCharacters = """
  			"isMain": false,
  			"name": "Grand Moff Tarkin",
  			"image_url": "https://upload.wikimedia.org/wikipedia/en/thumb/5/5a/Grand_Moff_Tarkin.png/330px-Grand_Moff_Tarkin.png",
- 			"gender": ""
+ 			"gender": "male"
  		},
  		{
  			"id": 56,
@@ -517,7 +517,7 @@ const val getCharacters = """
  			"id": 61,
  			"isMain": false,
  			"name": "Ki-Adi-Mundi",
- 			"image_url": "",
+ 			"image_url": "https://m.media-amazon.com/images/M/MV5BMTMxNTEzMTkyM15BMl5BanBnXkFtZTcwNzU1NzI4NA@@._V1_SY1000_CR0,0,664,1000_AL_.jpg",
  			"gender": ""
  		},
  		{
@@ -636,7 +636,7 @@ const val getCharacters = """
  			"id": 78,
  			"isMain": false,
  			"name": "Saesee Tiin",
- 			"image_url": "",
+ 			"image_url": "https://m.media-amazon.com/images/M/MV5BMTg3MTM5ODA3OV5BMl5BanBnXkFtZTcwMTA2NzI4NA@@._V1_.jpg",
  			"gender": ""
  		},
  		{
@@ -650,7 +650,7 @@ const val getCharacters = """
  			"id": 80,
  			"isMain": false,
  			"name": "Shmi Skywalker",
- 			"image_url": "",
+ 			"image_url": "https://m.media-amazon.com/images/M/MV5BMjE0MDM2MzkzOF5BMl5BanBnXkFtZTcwNTkyMzA4NA@@._V1_SY1000_SX1500_AL_.jpg",
  			"gender": ""
  		},
  		{
@@ -800,7 +800,7 @@ const val getFilms = """
         "name" : "The Phantom Menace",
         "complete_name": "Star Wars: Episode I - The Phantom Menace (1999)",
         "launch_date": "1999-05-19",
-        "character_ids": [0],
+        "character_ids": [0,14,3,12,80,59,8,7,6,43,19,42,13,83],
         "synopsis" : "Two Jedi escape a hostile blockade to find allies and come across a young boy who may bring balance to the Force, but the long dormant Sith resurface to claim their old glory.",
         "director": "George Lucas",
         "cover_album" : "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY999_SX666_AL_.jpg",
@@ -811,7 +811,7 @@ const val getFilms = """
         "name" : "Attack of the Clones",
         "complete_name": "Star Wars: Episode II - Attack of the Clones (2002)",
         "launch_date": "16 de maio de 2002",
-        "character_ids": [0],
+        "character_ids": [0,3,12,23,13,6,80,22,36,59,89,8,61,20,7],
         "synopsis" : "Ten years after initially meeting, Anakin Skywalker shares a forbidden romance with Padmé Amidala, while Obi-Wan Kenobi investigates an assassination attempt on the senator and discovers a secret clone army crafted for the Jedi.",
         "director": "George Lucas",
         "cover_album" : "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SY999_CR0,0,659,999_AL_.jpg",
@@ -822,7 +822,7 @@ const val getFilms = """
         "name" : "Revenge of the Sith",
         "complete_name": "Star Wars: Episode III - Revenge of the Sith (2005)",
         "launch_date": "2005-05-19",
-        "character_ids": [0],
+        "character_ids": [3,12,0,25,13,36,6,8,23,61,55,35,7,11,33,78,31,24],
         "synopsis" : "Three years into the Clone Wars, the Jedi rescue Palpatine from Count Dooku. As Obi-Wan pursues a new threat, Anakin acts as a double agent between the Jedi Council and Palpatine and is lured into a sinister plan to rule the galaxy.",
         "director": "George Lucas",
         "cover_album" : "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SY1000_SX750_AL_.jpg",
@@ -833,7 +833,7 @@ const val getFilms = """
         "name" : "The Force Awakens",
         "complete_name": "Star Wars: Episode VII - The Force Awakens (2015)",
         "launch_date": "2015-12-18",
-        "character_ids": [0],
+        "character_ids": [10,4,5,29,26,17,27,30,53,8,11],
         "synopsis" : "Three decades after the Empire's defeat, a new threat arises in the militant First Order. Defected stormtrooper Finn and the scavenger Rey are caught up in the Resistance's search for the missing Luke Skywalker.",
         "director": "J.J. Abrams",
         "cover_album" : "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SY1000_CR0,0,677,1000_AL_.jpg",
@@ -844,7 +844,7 @@ const val getFilms = """
         "name" : "The Last Jedi",
         "complete_name": "Star Wars: Episode VIII - The Last Jedi (2017)",
         "launch_date": "2017-12-14",
-        "character_ids": [0],
+        "character_ids": [4,5,29,26,17,30,53,8,16,15,6,11,7,9],
         "synopsis" : " Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares for battle with the First Order.",
         "director": "Rian Johnson",
         "cover_album" : "https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
@@ -855,7 +855,7 @@ const val getFilms = """
         "name" : "The Rise of Skywalker",
         "complete_name": "Star Wars: Episode IX - The Rise of Skywalker (2019)",
         "launch_date": "2019-12-19",
-        "character_ids": [0],
+        "character_ids": [5,4,29,26,17,8,53,27,76,25,18,7,9,27,0,35,30,64,13,31,3,32,6,32,14,10],
         "synopsis" : "The surviving members of the resistance face the First Order once again, and the legendary conflict between the Jedi and the Sith reaches its peak bringing the Skywalker saga to its end.",
         "director": "J.J. Abrams",
         "cover_album" : "https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
