f90d0732d947fd7d42890beeb4225d17eff2d622
eccard <felipelugao@id.uff.br>
2020-04-26 17:43:21 -0300

    Removes unnecessary import

--- app/src/main/java/com/eccard/starwarscharacters/ui/flimdetail/FilmDetailFrg.kt
+++ app/src/main/java/com/eccard/starwarscharacters/ui/flimdetail/FilmDetailFrg.kt
@@ -4,7 +4,6 @@ import android.app.Dialog
 import android.content.pm.ActivityInfo
 import android.content.res.Configuration
 import android.os.Bundle
-import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
