947e5a77cef6671a6719b2843cdfed908132a4d6
eccard <felipelugao@id.uff.br>
2020-04-26 21:35:42 -0300

    add test for getFilms

--- app/src/test/java/com/eccard/starwarscharacters/data/api/StarWarsApiTest.kt
+++ app/src/test/java/com/eccard/starwarscharacters/data/api/StarWarsApiTest.kt
@@ -2,6 +2,7 @@ package com.eccard.starwarscharacters.data.api
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.eccard.starwarscharacters.data.model.Charactter
+import com.eccard.starwarscharacters.data.model.Film
 import com.eccard.starwarscharacters.util.Constants
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
@@ -99,7 +100,80 @@ class StarWarsApiTest {
 
     }
 
+    @Test
+    fun getFilms(){
+        enqueueResponse("films_response.json")
+        val call = starWarsApi.getFilms().execute()
+        val request = mockWebServer.takeRequest()
+        Assert.assertThat(request.path, `is`("/"+Constants.FILMS))
 
+        Assert.assertThat<FilmRespose>(call.body(), IsNull.notNullValue())
+        Assert.assertThat(call.body()!!.total, `is`(3))
+
+        val film1977 = call.body()!!.items[0]
+        film1977.checkInfos(
+            0,
+            "A New Hope",
+            "Star Wars: Episode IV - A New Hope (1977)",
+            "1977-05-25",
+            arrayListOf(4,10,5,55,3,8,7,11,0),
+            "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
+            "George Lucas",
+            "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,643,1000_AL_.jpg",
+            "https://www.youtube.com/watch?v=1g3_CFmnU7k"
+        )
+
+        val film1980 = call.body()!!.items[1]
+        film1980.checkInfos(
+            1,
+            "The Empire Strikes Back",
+            "Star Wars: Episode V - The Empire Strikes Back (1980)",
+            "1980-05-21",
+            arrayListOf(4,10,5,8,0,11,7,6,3,20,63),
+            "After the Rebels are brutally overpowered by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda, while his friends are pursued by Darth Vader and a bounty hunter named Boba Fett all over the galaxy.",
+            "Irvin Kershner",
+            "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,641,1000_AL_.jpg",
+            "https://www.youtube.com/watch?v=JNwNXF9Y6kY"
+        )
+
+        val film1983 = call.body()!!.items[2]
+        film1983.checkInfos(
+            2,
+            "Return of the Jedi",
+            "Star Wars: Episode VI - Return of the Jedi (1983)",
+            "1983-05-25",
+            arrayListOf(4,10,5,18,8,11,0,6,3,7,38,65,90,20),
+            "After a daring mission to rescue Han Solo from Jabba the Hutt, the Rebels dispatch to Endor to destroy the second Death Star. Meanwhile, Luke struggles to help Darth Vader back from the dark side without falling into the Emperor's trap.",
+            "Richard Marquand",
+            "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY999_CR0,0,644,999_AL_.jpg",
+            "https://www.youtube.com/watch?v=5UfA_aKBGMc"
+        )
+
+    }
+
+    fun Film.checkInfos(expectedId : Int,
+                        expectedName : String,
+                        expectedCompleteName : String,
+                        expectedLaunchDate : String,
+                        expectedCharsIds : List<Int>,
+                        expectedSynopsis : String,
+                        expectedDirector : String,
+                        expectedCoverAlbum : String,
+                        expectedTrailer : String) {
+        Assert.assertThat(this.id,`is`(expectedId))
+        Assert.assertThat(this.name, `is`(expectedName))
+        Assert.assertThat(this.completeName, `is`(expectedCompleteName))
+        Assert.assertThat(this.launchDate, `is`(expectedLaunchDate))
+        Assert.assertThat(this.synopsis, `is`(expectedSynopsis))
+        Assert.assertThat(this.director, `is`(expectedDirector))
+        Assert.assertThat(this.coverAlbum, `is`(expectedCoverAlbum))
+        Assert.assertThat(this.trailer, `is`(expectedTrailer))
+
+        Assert.assertThat(this.charactersIds.size,`is`(expectedCharsIds.size))
+
+        for (realmInt in this.charactersIds){
+            Assert.assertThat(expectedCharsIds.contains(realmInt._value),`is`(true))
+        }
     }
 
     fun Charactter.checkCharacter(expectedId : Int,
