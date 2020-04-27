2635c8e753b9fae88de4e466ce1c2b1b7ad1c0e6
eccard <felipelugao@id.uff.br>
2020-04-27 20:26:58 -0300

    Implement FIlmsViewModel test

---
+++ app/src/test/java/com/eccard/starwarscharacters/ui/films/FilmsViewModelTest.kt
@@ -0,0 +1,44 @@
+package com.eccard.starwarscharacters.ui.films
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.eccard.starwarscharacters.data.Repository
+import com.eccard.starwarscharacters.data.model.CharacterAdapterPojo
+import com.eccard.starwarscharacters.data.model.Film
+import com.eccard.starwarscharacters.ui.flimdetail.FilmDetailViewModel
+import com.eccard.starwarscharacters.util.getFilmResponse
+import com.eccard.starwarscharacters.util.mock
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito
+
+@RunWith(JUnit4::class)
+class FilmsViewModelTest {
+
+    @Rule
+    @JvmField
+    val instantExecutor = InstantTaskExecutorRule()
+    private val repository = Mockito.mock(Repository::class.java)
+    private lateinit var viewModel: FilmsViewModel
+
+
+    @Test
+    fun getFilms() {
+        val resultLive = MutableLiveData<List<Film>>()
+        resultLive.value = getFilmResponse().items
+        Mockito.`when`(repository.loadAllFilms()).thenReturn(resultLive)
+        viewModel = FilmsViewModel(repository)
+
+        val observer = mock<Observer<List<Film>>>()
+        viewModel.films.observeForever(observer)
+        Mockito.verify(observer).onChanged(resultLive.value)
+    }
+
+}
