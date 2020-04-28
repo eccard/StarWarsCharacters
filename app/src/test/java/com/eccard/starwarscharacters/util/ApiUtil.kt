0c7ed756c3cd90576a39f75832ecba2a5e88b02b
eccard <felipelugao@id.uff.br>
2020-04-27 23:21:17 -0300

    Refactor util test calss to common directory

--- app/src/test/java/com/eccard/starwarscharacters/util/ApiUtil.kt
+++ app/src/test/java/com/eccard/starwarscharacters/util/ApiUtil.kt
@@ -9,6 +9,3 @@ import org.mockito.Mockito
 inline fun <reified T> mock(): T = Mockito.mock(T::class.java)
 
 inline fun <reified T> argumentCaptor(): ArgumentCaptor<T> = ArgumentCaptor.forClass(T::class.java)
-
-object ApiUtil {
-}
